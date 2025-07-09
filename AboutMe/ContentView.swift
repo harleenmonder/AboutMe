97b9e687e79997a916d684b9a02a7f6e1c879df1
Harleen Monder <harleenmonder@gmail.com>
2025-07-10 00:49:07 +0200

    Added images, text, colour and formatted page

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack() {
+                Text("HARLEEN MONDER")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color(hue: 0.762, saturation: 0.707, brightness: 0.725))
+                HStack {
+                    Image("Luna")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.all)
+                    
+                    Text("Luna 🩶")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                        .padding(.vertical)
+                    
+                }
+                
+                HStack {
+                    Image("kwk")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.all)
+                        
+                    Text("KWK 💚")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                        .padding(.horizontal)
+                }
+            }
         }
         .padding()
+        .background(Rectangle() .foregroundColor(Color(hue: 0.768, saturation: 0.205, brightness: 0.956)))
+        .cornerRadius(20)
+        .shadow(radius: 20)
+        .border(Color.white, width: 3)
+        .padding()
+        
+        
     }
 }
 
