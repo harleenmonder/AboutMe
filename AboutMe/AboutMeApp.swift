42deb07042ac41ed83cfabc7a7840fa43cfa9ca9
Harleen Monder <harleenmonder@gmail.com>
2025-07-09 17:37:02 +0200

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Harleen Monder on 2025-07-09.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
